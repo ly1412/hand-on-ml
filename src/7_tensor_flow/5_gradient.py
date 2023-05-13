782cc8268aefa5abcbb024a730257b7c94268038
ly1412 <1079752@qq.com>
2023-05-13 22:28:18 +0800

    feature num bug

--- src/7_tensor_flow/5_gradient.py
+++ src/7_tensor_flow/5_gradient.py
@@ -19,6 +19,7 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(HOUSING_PATH, "housing.csv")
     return pd.read_csv(csv_path)
 
+
 housing = load_housing_data()
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 # 按重要属性分层并进行split
@@ -50,25 +51,29 @@ num_pipeline = Pipeline([
 ])
 
 scaled_housing_data_plus_bias = num_pipeline.fit_transform(housing)
-print(scaled_housing_data_plus_bias.shape)
+print("scaled shape:", scaled_housing_data_plus_bias.shape)
+m, n = scaled_housing_data_plus_bias.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data_plus_bias]
+print("add bais shape:", housing_data_plus_bias.shape)
 tf.compat.v1.disable_eager_execution()
-n_epochs = 1000
+n_epochs = 3000
 learning_rate = 0.01
-X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
-y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
-m, n = X.shape
-theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
-y_pred = tf.matmul(X, theta, name="predictions")
+X = tf.compat.v1.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.compat.v1.constant(housing_labels.to_numpy().reshape(-1, 1), dtype=tf.float32, name="y")
+
+theta = tf.compat.v1.Variable(tf.compat.v1.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
+y_pred = tf.compat.v1.matmul(X, theta, name="predictions")
 error = y_pred - y
-mse = tf.reduce_mean(tf.square(error), name="mse")
-gradients = 2/m * tf.matmul(tf.transpose(X), error)
-training_op = tf.assign(theta, theta - learning_rate * gradients)
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-     sess.run(init)
-     for epoch in range(n_epochs):
-         if epoch % 100 == 0:
-             print("Epoch", epoch, "MSE =", mse.eval())
-         sess.run(training_op)
-     best_theta = theta.eval()
+print("error shape:", error.shape)
+mse = tf.compat.v1.reduce_mean(tf.compat.v1.square(error), name="mse")
+gradients = 2/m * tf.compat.v1.matmul(tf.compat.v1.transpose(X), error)
+training_op = tf.compat.v1.assign(theta, theta - learning_rate * gradients)
+init = tf.compat.v1.global_variables_initializer()
+with tf.compat.v1.Session() as sess:
+    sess.run(init)
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch:", epoch, ",MSE:", mse.eval())
+        sess.run(training_op)
+    best_theta = theta.eval()
 print(best_theta)
